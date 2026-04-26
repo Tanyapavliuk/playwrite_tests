8d494424ea91991a5b5cfb145a3700807a411c95
Tanya Pavliuk <122897433+Tanyapavliuk@users.noreply.github.com>
2026-04-26 22:20:09 +0300

    Code refactoring

--- tests/mainPage.spec.ts
+++ tests/mainPage.spec.ts
@@ -1,38 +1,66 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 
-test('Main menu, Header is visible:', async ({ page }) => {
-  await page.goto('https://tetiana-prylepska-qa.vercel.app/');
-  await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'About me' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Work experience' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'Portfolio' })).toBeVisible();
-  await expect(page.getByRole('navigation').getByRole('link', { name: 'Contact me' })).toBeVisible();
+const elements = [
+  {
+    locator: (page:Page) => page.getByRole('link', { name: 'Home' }),
+    name: 'Home',
+    anchor: '/'
+  },
+  {
+    locator: (page:Page) => page.getByRole('link', { name: 'About me' }),
+    name: 'About me',
+    anchor: '/#about'
+  },
+  {
+    locator: (page:Page) => page.getByRole('link', { name: 'Work experience' }),
+    name: 'Work experience',
+    anchor: '/#work'
+  },
+  {
+    locator: (page:Page) => page.getByRole('link', { name: 'Portfolio' }),
+    name: 'Portfolio',
+    anchor: '/portfolio'
+  },
+  {
+    locator: (page:Page) => page.getByRole('navigation').getByRole('link', { name: 'Contact me' }),
+    name: 'Contact me',
+    anchor: '/#contact'
+  }
+];
+
+test.describe('Test for navigation on the main page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://tetiana-prylepska-qa.vercel.app/');
+  });
+
+  test('Navigation: Verification of text, link of all nav elements', async ({
+    page
+  }) => {
+    elements.forEach(({ locator, name, anchor }) => {
+      test.step(`Check element ${name}`, async () => {
+        await expect.soft(locator(page)).toBeVisible();
+        await expect.soft(locator(page)).toContainText(name);
+        await expect.soft(locator(page)).toHaveAttribute('href', anchor);
+
+      });
+    });
+  });
 });
 
-test('Main menu, correct names:', async ({ page }) => {
-  await page.goto('https://tetiana-prylepska-qa.vercel.app/');
-  await expect(page.getByRole('navigation')).toContainText('Home');
-  await expect(page.getByRole('navigation')).toContainText('About me');
-  await expect(page.getByRole('navigation')).toContainText('Work experience');
-  await expect(page.getByRole('navigation')).toContainText('Portfolio');
-  await expect(page.getByRole('navigation')).toContainText('Contact me');
-  await expect(page.getByRole('link', { name: 'View test documentation' })).toBeVisible();
-  await expect(page.getByRole('link', { name: 'View test documentation' })).toContainText('View test documentation');
-});
+test.describe('Test for the hero section on the main page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://tetiana-prylepska-qa.vercel.app/');
+  });
 
-test('Main menu, links:', async ({ page }) => {
-  await page.goto('https://tetiana-prylepska-qa.vercel.app/');
-
-  await expect(page.getByRole('link', { name: 'Home' })).toHaveAttribute('href', '/');
-  await expect(page.getByRole('link', { name: 'About me' })).toHaveAttribute('href', '/#about');
-  await expect(page.getByRole('link', { name: 'Work experience' })).toHaveAttribute('href', '/#work');
-  await expect(page.getByRole('link', { name: 'Portfolio' })).toHaveAttribute('href', '/portfolio');
-});
-
-test('Correct link:', async ({ page }) => {
-  await page.goto('https://tetiana-prylepska-qa.vercel.app/');
-
-  await expect.soft(page.getByRole('link', { name: 'View test documentation' })).toBeVisible();
-  await expect.soft(page.getByRole('link', { name: 'View test documentation' })).toContainText('View test documentation');
-  await expect.soft(page.getByRole('link', { name: 'View test documentation' })).toHaveAttribute('href', '/test-documentation');
+  test('Element verification View test documentatio:', async ({ page }) => {
+    await expect
+      .soft(page.getByRole('link', { name: 'View test documentation' }))
+      .toBeVisible();
+    await expect
+      .soft(page.getByRole('link', { name: 'View test documentation' }))
+      .toContainText('View test documentation');
+    await expect
+      .soft(page.getByRole('link', { name: 'View test documentation' }))
+      .toHaveAttribute('href', '/test-documentation');
+  });
 });
