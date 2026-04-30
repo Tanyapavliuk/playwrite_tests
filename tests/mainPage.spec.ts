37575b718d7a19c547b322e5d612a52941662d1c
Tanya Pavliuk <122897433+Tanyapavliuk@users.noreply.github.com>
2026-04-30 22:25:06 +0300

    Add models, refactor

--- tests/mainPage.spec.ts
+++ tests/mainPage.spec.ts
@@ -1,66 +1,19 @@
-import { test, expect, Page } from '@playwright/test';
+import { test, expect, Page, Locator } from '@playwright/test';
+import { MainPage } from './models/MainPage';
 
-const elements = [
-  {
-    locator: (page:Page) => page.getByRole('link', { name: 'Home' }),
-    name: 'Home',
-    anchor: '/'
-  },
-  {
-    locator: (page:Page) => page.getByRole('link', { name: 'About me' }),
-    name: 'About me',
-    anchor: '/#about'
-  },
-  {
-    locator: (page:Page) => page.getByRole('link', { name: 'Work experience' }),
-    name: 'Work experience',
-    anchor: '/#work'
-  },
-  {
-    locator: (page:Page) => page.getByRole('link', { name: 'Portfolio' }),
-    name: 'Portfolio',
-    anchor: '/portfolio'
-  },
-  {
-    locator: (page:Page) => page.getByRole('navigation').getByRole('link', { name: 'Contact me' }),
-    name: 'Contact me',
-    anchor: '/#contact'
-  }
-];
+let mainPage:MainPage
 
 test.describe('Test for navigation on the main page', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('https://tetiana-prylepska-qa.vercel.app/');
+  test.beforeEach(async ({page}) => {
+    mainPage = new MainPage(page)
+    await mainPage.openMainPage()
+  })
+
+  test('Navigation: Verification of text, link of all nav elements', async () => {
+    await mainPage.checkNavElements()
   });
 
-  test('Navigation: Verification of text, link of all nav elements', async ({
-    page
-  }) => {
-    elements.forEach(({ locator, name, anchor }) => {
-      test.step(`Check element ${name}`, async () => {
-        await expect.soft(locator(page)).toBeVisible();
-        await expect.soft(locator(page)).toContainText(name);
-        await expect.soft(locator(page)).toHaveAttribute('href', anchor);
-
-      });
-    });
-  });
-});
-
-test.describe('Test for the hero section on the main page', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('https://tetiana-prylepska-qa.vercel.app/');
-  });
-
-  test('Element verification View test documentatio:', async ({ page }) => {
-    await expect
-      .soft(page.getByRole('link', { name: 'View test documentation' }))
-      .toBeVisible();
-    await expect
-      .soft(page.getByRole('link', { name: 'View test documentation' }))
-      .toContainText('View test documentation');
-    await expect
-      .soft(page.getByRole('link', { name: 'View test documentation' }))
-      .toHaveAttribute('href', '/test-documentation');
+  test('Verification: Of buttons on the Hero section', async ()=>{
+    await mainPage.checkHeroButtons()
   });
 });
---
+++ tests/models/MainPage.ts
@@ -0,0 +1,98 @@
+import test, { expect, Locator, Page } from '@playwright/test';
+
+interface ElementsNav {
+  locator: (page: Page) => Locator;
+  name: string;
+  anchor: string;
+}
+
+interface HeroButtons {
+    locator: (page: Page) => Locator;
+    name: string;
+    attributes: {
+                type: string, 
+                text: string
+            }
+}
+
+export class MainPage {
+  readonly page: Page;
+  readonly elementsNav: ElementsNav[];
+  readonly heroButtons : HeroButtons[];
+
+  constructor(page: Page) {
+    this.page = page;
+    this.elementsNav = [
+      {
+        locator: (page: Page) => page.getByRole('link', { name: 'Home' }),
+        name: 'Home',
+        anchor: '/',
+      },
+      {
+        locator: (page: Page) => page.getByRole('link', { name: 'About me' }),
+        name: 'About me',
+        anchor: '/#about',
+      },
+      {
+        locator: (page: Page) =>
+          page.getByRole('link', { name: 'Work experience' }),
+        name: 'Work experience',
+        anchor: '/#work',
+      },
+      {
+        locator: (page: Page) => page.getByRole('link', { name: 'Portfolio' }),
+        name: 'Portfolio',
+        anchor: '/portfolio',
+      },
+      {
+        locator: (page: Page) =>
+          page
+            .getByRole('navigation')
+            .getByRole('link', { name: 'Contact me' }),
+        name: 'Contact me',
+        anchor: '/#contact',
+      },
+    ];
+    this.heroButtons = [
+        {
+            locator: (page: Page) => page.getByRole('link', { name: 'View test documentation' }),
+            name: 'View test documentation',
+            attributes: {
+                type: 'href', 
+                text: '/test-documentation'
+            }
+        }, 
+        {
+            locator: (page: Page) => page.getByRole('link', { name: 'Contact me' }).nth(1),
+            name: 'Contact me',
+            attributes: {
+                type: 'href', 
+                text: '/#contact'
+        } }
+    ]
+  }
+
+  async openMainPage () {
+    await this.page.goto('https://tetiana-prylepska-qa.vercel.app/')
+  }
+
+  async checkNavElements () {
+    for await (const {locator,name,anchor} of this.elementsNav) {
+        test.step(`Check element ${name}`, async () => {
+                await expect.soft(locator(this.page)).toBeVisible();
+                await expect.soft(locator(this.page)).toContainText(name);
+                await expect.soft(locator(this.page)).toHaveAttribute('href', anchor);
+        });
+    }
+  }
+
+  async checkHeroButtons () {
+    for await (const {locator,name, attributes } of this.heroButtons) {
+        test.step(`Check element ${name}`, async () => {
+                await expect.soft(locator(this.page)).toBeVisible();
+                await expect.soft(locator(this.page)).toContainText(name);
+                await expect.soft(locator(this.page)).toHaveAttribute(attributes.type, attributes.text);
+        });
+    }
+  }
+}
